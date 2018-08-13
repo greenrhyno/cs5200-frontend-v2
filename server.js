5be6e3d4457406437affe1767f093a8db2c8ae6d
Ryan <green.ry@husky.neu.edu>
2018-08-13 17:49:54 -0400

    Another typo

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/cs5200-frontend-vs'));
+app.use(express.static(__dirname + '/dist/cs5200-frontend-v2'));
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join{__dirname + '/dist/cs5200-fronted-vs/index.html'});
+	res.sendFile(path.join{__dirname + '/dist/cs5200-frontend-v2/index.html'});
 });
 
 app.listen(process.env.PORT || 4200);
