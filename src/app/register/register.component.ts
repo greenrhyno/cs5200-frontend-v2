98aa311628c0ab3622c5973bbbbc1dad7dcbc1a6
lockhartc <lockhart.c@husky.neu.edu>
2018-08-17 19:17:57 -0400

    profile, register, login updates.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PersonServiceClient} from '../services/person.service.client';
-
+import {Router} from '@angular/router';
+let selfReference;
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,15 +12,28 @@ export class RegisterComponent implements OnInit {
   username;
   password;
   confirmPassword;
-  constructor(private personService: PersonServiceClient) {}
-  register(username, password, confirmPassword) {
-    if (password.toString() !== confirmPassword.toString()) {
-      alert('Passwords do not match, please try again.');
-    } else {
-      this.personService.createPerson(username, password)
-        .then(response => response.json);
-    }
+  typeOfUser;
+
+  constructor(private router: Router, private personService: PersonServiceClient) {
+    selfReference = this;
   }
+  register(username, password, confirmPassword) {
+    this.personService.findPersonByLogin(username)
+      .then( response => {
+        if (response !== null) {
+          alert('Sorry, that user name is already taken.');
+        } else {
+          if (password.toString() !== confirmPassword.toString()) {
+            alert('Passwords do not match, please try again.');
+          } else {
+            this.personService.createPerson(username, password, this.typeOfUser)
+              .then(r => r.json);
+            this.personService.current(username);
+            this.personService.currentType(this.typeOfUser);
+            selfReference.router.navigate(['profile']);
+        }
+        } }
+      ); }
   ngOnInit() {
   }
 
